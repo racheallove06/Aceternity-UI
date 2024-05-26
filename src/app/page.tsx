24a20aada76910a21fc36ebc903e9977598ddc7e
rachealmuchiri <rachealloveo6@gmail.com>
2024-05-26 10:19:47 +0300

    added a new folder and file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="relative bg-black">
       <div>
         <h1>Hello, Portfolio</h1>
       </div>
