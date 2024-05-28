a835dc531125d996c49d9f37d9296a4ab05fe880
rachealmuchiri <rachealloveo6@gmail.com>
2024-05-28 10:13:02 +0300

    imported the page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { navItems } from "@/data";
+import Hero from "./components/Hero";
+import { FloatingNav } from "./components/ui/FloatingNavbar";
+import Grid from "./components/Grid";
+import RecentProjects from "./components/RecentProjects";
+import Clients from "./components/Clients";
+import Experience from "./components/Experience";
+import Approach from "./components/Approach";
+import Footer from "./components/Footer";
+
+const Home = () => {
   return (
-    <main className="relative bg-black">
-      <div>
-        <h1>Hello, Portfolio</h1>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
-}
+};
+
+export default Home;
