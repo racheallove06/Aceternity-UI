ab23e4cf1828b0d1b86d677a2c052eaf30dcaa37
rachealmuchiri <rachealloveo6@gmail.com>
2024-05-28 10:05:25 +0300

    added the spotlight

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Spotlight } from "./ui/Spotlight";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="pb-20 pt-36">
+      <Spotlight
+        className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+        fill="white"
+      />
+      <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="purple" />
+      <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+    </div>
+  );
 };
 
 export default Hero;
